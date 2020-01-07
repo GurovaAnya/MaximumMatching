e98121af88eeb078c49dc831e2f778602cf2d6e0
Анна Гурова <aagurova_2@edu.hse.ru>
2020-01-07 19:15:02 +0500

    Добавьте файлы проекта.

---
+++ MaximumMatching/Graph.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections.Generic;
+
+namespace MaximumMatching
+{
+    class Graph
+    {
+        private int[] marks1;
+        private int[] marks2;
+        private int[] pairs; 
+        List<int>[] half1;
+        List<int>[] half2;
+        int size;
+
+        /// <summary>
+        /// Конструктор графа
+        /// </summary>
+        /// <param name="size"> количество вершин в одной доли </param>
+        public Graph(int size)
+        {
+            this.size = size;
+            marks1 = new int[size];
+            marks2 = new int[size];
+            pairs = new int[size];
+            
+            half1 = new List<int>[size];
+            half2 = new List<int>[size];
+            for (int i = 0; i < size; i++)
+            {
+                marks1[i] = -1;
+                marks2[i] = -1;
+                pairs[i] = -1;
+            }
+        }
+
+        /// <summary>
+        /// Доавление связей
+        /// </summary>
+        /// <param name="num"> Номер вершины первой доли </param>
+        /// <param name="connections"> Номера вершин второй доли, с которой соединена эта вершина</param>
+        public void AddConnection(int num, IEnumerable<int> connections)
+        {
+            half1[num] = new List<int>(connections);
+            foreach (int con in connections)
+            {
+                if (half2[con] == null)
+                    half2[con] = new List<int>();
+                half2[con].Add(num);
+            }
+        }
+
+        /// <summary>
+        /// Метод, находящий перосочетания
+        /// </summary>
+        /// <returns>
+        /// Массив со связями вершины из второй доли с вершиной из первой доли 
+        /// i-тый элемент массива - это вершина первой доли, с которой соединена i-тая вершина второй доли
+        /// </returns>
+        public int [] FindPairs()
+        {
+            bool foundMatching;
+            
+            while (true)
+            {
+                List<int> curNodes1 = new List<int>();
+                List<int> curNodes2 = new List<int>();
+
+                List<int> pairsList = new List<int>((IEnumerable<int>)pairs);
+
+                for (int i = 0; i < size; i++)
+                {
+                    if (!pairsList.Contains(i))
+                        curNodes1.Add(i); 
+                }
+
+                for (int i = 0; i < size; i++)
+                {
+                    marks1[i] = -1;
+                    marks2[i] = -1;
+                }
+
+                do
+                {
+
+                    foundMatching = false;
+\
+                    FirstHalfWork(ref curNodes1, ref curNodes2, out foundMatching);
+                    curNodes1.Clear();
+                    if (foundMatching)
+                        break;
+                    if (curNodes2.Count == 0) 
+                        return pairs;
+                    SecondHalfWork(ref curNodes1, ref curNodes2);
+                    curNodes2.Clear();
+
+                } while (curNodes1.Count > 0);
+            }
+        }
+
+        private void FirstHalfWork(ref List<int> curNodes1, ref List<int> curNodes2, out bool foundMatching)
+        {
+            foreach (int node in curNodes1)
+            {
+                foreach (int child in half1[node])
+                    if (marks2[child] == -1&&pairs[child]!=node)
+                    {
+                        marks2[child] = node;
+                        if (pairs[child] == -1)
+                        {
+                            foundMatching = true;
+                            ReverseChain(child);
+                            return;
+                        }
+                        curNodes2.Add(child);
+                    }
+            }
+            foundMatching = false;
+        }
+
+        private void SecondHalfWork(ref List<int> curNodes1, ref List<int> curNodes2)
+        {
+            foreach (int node in curNodes2)
+            {
+                foreach (int child in half2[node])
+                    if (marks1[child] == -1&&pairs[node]==child)
+                    {
+                        marks1[child] = node;
+                        curNodes1.Add(child);
+                    }
+            }
+        }
+
+
+        private void ReverseChain(int child)
+        {
+            int currNode = child;
+            while (currNode!=-1)
+            {
+                pairs[currNode] = marks2[currNode];
+                currNode = marks1[marks2[currNode]];
+
+            }
+        }
+    }
+}
---
+++ MaximumMatching/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MaximumMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph gr = new Graph(5);
+            gr.AddConnection(0, new int[] { 0, 1, 3 });
+            gr.AddConnection(1, new int[] { 1, 2 });
+            gr.AddConnection(2, new int[] { 1, 2 });
+            gr.AddConnection(3, new int[] { 0, 2, 3, 4 });
+            gr.AddConnection(4, new int[] { 1, 2 });
+            Print(gr.FindPairs());
+
+            Graph gr2 = new Graph(5);
+            gr2.AddConnection(0, new int[] { 0,1,3 });
+            gr2.AddConnection(1, new int[] { 2,4});
+            gr2.AddConnection(2, new int[] { 0,1,3 });
+            gr2.AddConnection(3, new int[] { 0,4 });
+            gr2.AddConnection(4, new int[] { 2,4 });
+            Print(gr2.FindPairs());
+
+            Graph gr3 = new Graph(0);
+            Print(gr3.FindPairs());
+
+
+        }
+
+        static void Print(int[] pairs)
+        {
+            for (int i = 0; i < pairs.Length; i++)
+            {
+                Console.Write(pairs[i] + 1);
+                Console.Write('-');
+                Console.WriteLine(i + 1);
+            }
+            Console.WriteLine(); 
+        }
+    }
+}
